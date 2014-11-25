8c44ebfda0f713c85df8b55ec54a01bdfe445927
Griff Hall <griffhall1@gmail.com>
2014-11-25 10:21:06 -0600

    Initial Commit

---
+++ table_viewsTests/table_viewsTests.swift
@@ -0,0 +1,36 @@
+//
+//  table_viewsTests.swift
+//  table_viewsTests
+//
+//  Created by Griff Hall on 11/25/14.
+//  Copyright (c) 2014 Griffappolis. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class table_viewsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
